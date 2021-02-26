5dab8ad7afebd9550a647061a1b869f08852b049
Dimitrios Tsapnidis <dimitrios.tsapnidis@gmail.com>
2021-02-26 09:33:48 +0200

    Add version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="example-pkg-dtsap", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Dimitrios Tsapnidis",
     author_email="dimitrios.tsapndis@gmail.com",
     description="A small example package",
